2878b720fc887bd85c0f593fded96543b5764d30
Rafa-Oliv <rafa.oli.rib@gmail.com>
2024-07-16 20:26:37 -0300

    coloquei mais funções no código

--- calculadoraIMC.py
+++ calculadoraIMC.py
@@ -1,5 +1,53 @@
 import PySimpleGUI as sg
 
+#calcumar IMC
+def calcular_Imc():
+    
+    while True:
+        try:
+            peso = float(values['peso'])
+            altura = float(values['altura'])
+            imc = round(peso/(altura**2),2)
+            return imc
+        except:
+            sg.popup('''
+                Digite apenas números
+                Coloque o ponto(.)
+                no lugar da vígula(,)
+            ''')
+            window['peso'].update('')
+            window['altura'].update('')
+            break
+            
+#Definir o diagnóstico
+def definir_diagnostico(imc):
+    
+    if imc < 17:
+        return 'Muito abaixo do peso'
+    elif 18.5 > imc >= 17:
+        return 'Abaixo do peso'
+    elif  25 > imc >= 18.5 :
+        return 'Normal'
+    elif 30 > imc >= 25:
+        return 'Acima do peso'
+    elif 35 > imc >= 30:
+        return 'Obesidade I'
+    elif 40 > imc >= 35:
+         return 'Obesidade II(Severa)'
+    else:
+        return 'Obesidade III(Mórbida)'
+
+#Definir a cor do texto        
+def definir_cor_texto(imc):
+    if imc < 18.5:
+        return 'white'
+    elif 25 > imc >= 18.5:
+        return 'green'
+    else:
+        return 'red'
+         
+
+
 sg.theme('Black')
 
 layout =[
@@ -8,84 +56,36 @@ layout =[
     [sg.Text('Digite sua altura(m)')],
     [sg.Input(key='altura')],
     [sg.Button(button_text='Calcular')],
-    [sg.Text(key='resultado')]
-   
-
+    [sg.Text(key='imc')],
+    [sg.Text(key='diagnostico')]
     ]
 
 window = sg.Window('Calculadora de IMC',layout=layout)
 
 while True:
+    
     event,values = window.read()
 
     if event == sg.WIN_CLOSED:
+        
         break
+    
     elif event == 'Calcular':
-        print('Calculado')
-        print(values)
-        try:
-            peso = float(values['peso'])
-            altura = float(values['altura'])
-            imc = round(peso/(altura**2),2)
-            if imc < 17:
-                resultado = f'''
-    IMC = {imc}
-    Muito abaixo do peso
-    '''
-                text_color = 'white'
-                
-            elif 18.5 > imc >= 17:
-                resultado = f'''
-    IMC = {imc}
-    Abaixo do peso
-    '''
-                text_color = 'white'
-            elif 18.5 > imc >= 17:
-                resultado = f'''
-    IMC = {imc}
-    Abaixo do peso
-    '''
-                text_color = 'white'
-
-            elif  25 > imc >= 18.5 :
-                resultado = f'''
-    IMC = {imc}
-    Peso Normal
-    '''
-                text_color = 'green'
-
-            elif 30 > imc >= 25:
-                resultado = f'''
-    IMC = {imc}
-    Acima do peso
-    '''
-                text_color = 'red'
-
-            elif 35 > imc >= 30:
-                resultado = f'''
-    IMC = {imc}
-    Obesidade I
-    '''
-                text_color = 'red'
-            elif 40 > imc >= 35:
-                resultado = f'''
-    IMC = {imc}
-    Obesidade II(Severa)
-    '''
-                text_color = 'red'
-            else:
-                resultado = f'''
-    IMC = {imc}
-    Obesidade III(Mórbida)
-    '''
-                text_color='red'
-                
-            window['resultado'].update(resultado)
-            window['resultado'].update(text_color=text_color)
-            print(resultado)
-        except:
-            sg.popup('''
-Digite apenas números
-coloque o ponto(.)
-no lugar da vígula
-''')
+        
+        imc = calcular_Imc()
+        
+        if imc:
+            
+            diagnostico = definir_diagnostico(imc)
+            
+            text_color = definir_cor_texto(imc)
+            
+            window['imc'].update(imc)
+            
+            window['imc'].update(text_color=text_color)
+            
+            window['diagnostico'].update(diagnostico)
+            
+            window['diagnostico'].update(text_color=text_color)
+            
+        
