c42f9a527405da300a2e174e014541e701edf5a1
Rafa-Oliv <rafa.oli.rib@gmail.com>
2024-07-17 14:05:07 -0300

    criei a função mostrar_erro e a função obter_diagnostico_e_cor e deletei a função definir_cor_texto

--- calculadoraIMC.py
+++ calculadoraIMC.py
@@ -1,8 +1,7 @@
 import PySimpleGUI as sg
 
-#calcumar IMC
-
-def calcular_Imc(window,values):
+# Calcular IMC
+def calcular_imc(window, values):
     
     while True:
         
@@ -12,121 +11,105 @@ def calcular_Imc(window,values):
             
             altura = float(values['altura'])
             
-            imc = round(peso/(altura**2),2)
+            imc = round(peso / (altura**2), 2)
             
             return imc
-        
+            
         except:
             
-            sg.popup('''
-                Digite apenas números
-                Coloque o ponto(.)
-                no lugar da vígula(,)
-            ''')
+            mostrar_erro()
             
             window['peso'].update('')
-
+            
             window['altura'].update('')
             
             break
-            
-#Definir o diagnóstico
-        
-def definir_diagnostico(imc):
+
+# Mostrar popup de erro
+def mostrar_erro():
+    
+    sg.popup('''
+        Digite apenas números
+        Coloque o ponto(.)
+        no lugar da vírgula(,)
+    ''')
+
+# Obter diagnóstico e cor do texto
+def obter_diagnostico_e_cor(imc):
     
     if imc < 17:
         
-        return 'Muito abaixo do peso'
-    
+        return 'Muito abaixo do peso', 'white'
+        
     elif 18.5 > imc >= 17:
         
-        return 'Abaixo do peso'
-    
-    elif  25 > imc >= 18.5 :
+        return 'Abaixo do peso', 'lightblue'
         
-        return 'Normal'
-    
-    elif 30 > imc >= 25:
-        
-        return 'Acima do peso'
-    
-    elif 35 > imc >= 30:
-        
-        return 'Obesidade I'
-    
-    elif 40 > imc >= 35:
-        
-         return 'Obesidade II(Severa)'
-        
-    else:
-        
-        return 'Obesidade III(Mórbida)'
-
-#Definir a cor do texto
-    
-def definir_cor_texto(imc):
-
-    if imc < 18.5:
-        
-        return 'white'
-    
     elif 25 > imc >= 18.5:
         
-        return 'green'
-    
+        return 'Normal', 'green'
+        
+    elif 30 > imc >= 25:
+        
+        return 'Acima do peso', 'yellow'
+        
+    elif 35 > imc >= 30:
+        
+        return 'Obesidade I', 'orange'
+        
+    elif 40 > imc >= 35:
+        
+        return 'Obesidade II (Severa)', 'red'
+        
     else:
         
-        return 'red'
+        return 'Obesidade III (Mórbida)', 'darkred'
 
-#criar_janela
-    
+# Criar janela
 def criar_janela():
-
+    
     sg.theme('Black')
-
-    layout =[
-        [sg.Text('Digite seu peso(Kg)')],
+    
+    layout = [
+        [sg.Text('Digite seu peso (Kg)')],
         [sg.Input(key='peso')],
-        [sg.Text('Digite sua altura(m)')],
+        [sg.Text('Digite sua altura (m)')],
         [sg.Input(key='altura')],
         [sg.Button(button_text='Calcular')],
         [sg.Text(key='imc')],
         [sg.Text(key='diagnostico')]
-        ]
-
-    return sg.Window('Calculadora de IMC',layout=layout)
-
-#executar código
-
-def main():
+    ]
     
+    return sg.Window('Calculadora de IMC', layout=layout)
+
+# Executar código
+def main():
     window = criar_janela()
 
     while True:
-        
-        event,values = window.read()
+        event, values = window.read()
 
         if event == sg.WIN_CLOSED:
-            
             break
-        
+            
         elif event == 'Calcular':
             
-            imc = calcular_Imc(window,values)
+            imc = calcular_imc(window, values)
             
             if imc:
                 
-                diagnostico = definir_diagnostico(imc)
+                diagnostico, text_color = obter_diagnostico_e_cor(imc)
                 
-                text_color = definir_cor_texto(imc)
+                window['imc'].update(imc)
                 
-                window['imc'].update(imc,text_color=text_color)
+                window['imc'].update(text_color=text_color)
                 
-                window['diagnostico'].update(diagnostico,text_color=text_color)
+                window['diagnostico'].update(diagnostico)
                 
-            
-                
-                
+                window['diagnostico'].update(text_color=text_color)
+
 if __name__ == '__main__':
-     main()
+    
+    main()
+
             
